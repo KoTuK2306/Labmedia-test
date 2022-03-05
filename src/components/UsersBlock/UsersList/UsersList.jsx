4bcf80dba5d7cc0758c543c66d2b8dc2ba1f18de
KoTuK2306 <dmitriy_aleshin_00@mail.ru>
2022-03-05 16:11:32 +0300

    feat: added users from database

--- src/components/UsersBlock/UsersList/UsersList.jsx
+++ src/components/UsersBlock/UsersList/UsersList.jsx
@@ -1,6 +1,17 @@
 import classes from "./UsersList.module.css";
+import { User } from "./User";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export const UsersList = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchdata = async () => {
+      const result = await axios("https://5ebbb8e5f2cfeb001697d05c.mockapi.io/users");
+      setUsers(result.data);
+    };
+    fetchdata();
+  }, []);
   return (
     <div className={classes.usersList}>
       <div className={classes.title}>
@@ -9,6 +20,9 @@ export const UsersList = () => {
         <p>Дата регистрации</p>
         <p>Рейтинг</p>
       </div>
+      {users.map((user) => (
+        <User key={user.id} user={user} />
+      ))}
     </div>
   );
 };
